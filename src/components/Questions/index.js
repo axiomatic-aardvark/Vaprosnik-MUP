5a22cdd045cfacdbd0d544f7ac638965e4f5352a
ppavlovski <pavlovskipetko@abv.bg>
2019-12-11 23:38:29 +0200

    finalized add Q look

--- src/components/Questions/index.js
+++ src/components/Questions/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import './style.scss';
 
-export default function index() {
-	return <div>questions</div>;
+export default function index(props) {
+	return (
+		<div>
+			<span
+				className="back"
+				onClick={() => {
+					props.history.push('/');
+				}}
+			>
+				Назад
+			</span>
+			<span>Моля опитайте по-късно...</span>
+		</div>
+	);
 }
