806135cf0c3af96bc51108fa8801508ac7156287
ppavlovski <pavlovskipetko@abv.bg>
2019-12-11 23:26:44 +0200

    adding questions to db

--- src/components/AddQuestion/index.js
+++ src/components/AddQuestion/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './style.scss';
 import { Form, Button } from 'react-bootstrap';
+import Done from '../../images/success.png';
 
 export default props => {
 	const [option1, setOption1] = useState('');
@@ -9,16 +10,22 @@ export default props => {
 	const [option3, setOption3] = useState('');
 	const [option4, setOption4] = useState('');
 	const [text, setText] = useState('');
+	const [isSubmitted, setIsSubmitted] = useState(false);
 
 	const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 
+	const onFormSubmit = () => {
+		pushQuestionToDB();
+		setIsSubmitted(true);
+	};
+
 	const pushQuestionToDB = () => {
 		axios
-			.post(proxyUrl + 'https://us-central1-atomic-swap.cloudfunctions.net/postBalance', {
-				option1: option1,
-				option2: option2,
-				option3: option3,
-				option4: option4,
+			.post(proxyUrl + 'https://us-central1-vaprosnik-mup.cloudfunctions.net/pushQuestionToDB', {
+				option1: option1 + ',t',
+				option2: option2 + ',f',
+				option3: option3 + ',f',
+				option4: option4 + ',f',
 				text: text,
 			})
 			.then(function(response) {
@@ -39,36 +46,43 @@ export default props => {
 			>
 				Назад
 			</span>
-			<Form>
-				<Form.Group controlId="formGroupEmail">
-					<Form.Label>Въпрос</Form.Label>
-					<Form.Control type="text" onChange={e => setText(e.target.value)}/>
-				</Form.Group>
-				<Form.Group controlId="formGroupPassword">
-					<Form.Label>Верен Отговор</Form.Label>
-					<Form.Control type="text" onChange={e => setOption1(e.target.value)}/>
-				</Form.Group>
-				<Form.Group controlId="formGroupPassword">
-					<Form.Label>Грешен Отговор 1</Form.Label>
-					<Form.Control type="text" onChange={e => setOption2(e.target.value)}/>
-				</Form.Group>
-				<Form.Group controlId="formGroupPassword">
-					<Form.Label>Грешен Отговор 2</Form.Label>
-					<Form.Control type="text" onChange={e => setOption3(e.target.value)}/>
-				</Form.Group>
-				<Form.Group controlId="formGroupPassword">
-					<Form.Label>Грешен Отговор 3</Form.Label>
-					<Form.Control type="text" onChange={e => setOption4(e.target.value)}/>
-				</Form.Group>
-				<Button
-					onClick={() => {
-						pushQuestionToDB();
-					}}
-					variant="primary"
-				>
-					Изпрати
-				</Button>
-			</Form>
+			{!isSubmitted ? (
+				<Form>
+					<Form.Group controlId="formGroupEmail">
+						<Form.Label>Въпрос</Form.Label>
+						<Form.Control type="text" onChange={e => setText(e.target.value)} />
+					</Form.Group>
+					<Form.Group controlId="formGroupPassword">
+						<Form.Label>Верен Отговор</Form.Label>
+						<Form.Control type="text" onChange={e => setOption1(e.target.value)} />
+					</Form.Group>
+					<Form.Group controlId="formGroupPassword">
+						<Form.Label>Грешен Отговор 1</Form.Label>
+						<Form.Control type="text" onChange={e => setOption2(e.target.value)} />
+					</Form.Group>
+					<Form.Group controlId="formGroupPassword">
+						<Form.Label>Грешен Отговор 2</Form.Label>
+						<Form.Control type="text" onChange={e => setOption3(e.target.value)} />
+					</Form.Group>
+					<Form.Group controlId="formGroupPassword">
+						<Form.Label>Грешен Отговор 3</Form.Label>
+						<Form.Control type="text" onChange={e => setOption4(e.target.value)} />
+					</Form.Group>
+					<Button
+						onClick={() => {
+							onFormSubmit();
+						}}
+						variant="primary"
+					>
+						Изпрати
+					</Button>
+				</Form>
+			) : (
+				<div className="done">
+					<img src={Done} alt="done-img"></img>
+					<span>Въпросът е успешно добавен!</span>
+				</div>
+			)}
 		</div>
 	);
 };
