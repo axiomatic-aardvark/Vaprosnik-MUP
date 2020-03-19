587eb92a051bac3be247b54507cfed5379b4776f
arthas168 <pavlovskipetko@abv.bg>
2020-03-19 20:40:51 +0200

    limit questions to 10.

--- src/components/GlobalState/actions.js
+++ src/components/GlobalState/actions.js
@@ -1,43 +1,57 @@
-import { UPDATE_QUESTIONS } from './types';
-import axios from 'axios';
+import { UPDATE_QUESTIONS } from "./types";
+import axios from "axios";
 
 export const questionsUpdate = async () => {
-	function shuffle(array) {
-		var currentIndex = array.length,
-			temporaryValue,
-			randomIndex;
+  function shuffle(array) {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
 
-		// While there remain elements to shuffle...
-		while (0 !== currentIndex) {
-			// Pick a remaining element...
-			randomIndex = Math.floor(Math.random() * currentIndex);
-			currentIndex -= 1;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
 
-			// And swap it with the current element.
-			temporaryValue = array[currentIndex];
-			array[currentIndex] = array[randomIndex];
-			array[randomIndex] = temporaryValue;
-		}
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
 
-		return array;
-	}
+    return array;
+  }
 
-	const getLimitedQuestions = arr => {
-		const scrambledQuestions = shuffle(arr);
-		return scrambledQuestions.slice(0, 20);
-	};
+  const affixWithAtSign = arr => {
+    console.log(arr);
+    // arr.forEach(element => {
+    //   console.log(element);
+    // });
 
-	try {
-		const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-		const res = await axios.get(proxyUrl + 'https://us-central1-vaprosnik-mup.cloudfunctions.net/getQuestions');
-		return {
-			type: UPDATE_QUESTIONS,
-			payload: { questions: getLimitedQuestions(res.data) },
-		};
-	} catch (error) {
-		return {
-			type: UPDATE_QUESTIONS,
-			payload: { questions: null },
-		};
-	}
+    return arr;
+  };
+
+  const getLimitedQuestions = arr => {
+    let scrambledQuestions = shuffle(arr);
+    scrambledQuestions = scrambledQuestions.slice(0, 10);
+
+    return affixWithAtSign(scrambledQuestions);
+  };
+
+  try {
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+    const res = await axios.get(
+      proxyUrl +
+        "https://us-central1-vaprosnik-mup.cloudfunctions.net/getQuestions"
+    );
+    return {
+      type: UPDATE_QUESTIONS,
+      payload: { questions: getLimitedQuestions(res.data) }
+    };
+  } catch (error) {
+    return {
+      type: UPDATE_QUESTIONS,
+      payload: { questions: null }
+    };
+  }
 };
