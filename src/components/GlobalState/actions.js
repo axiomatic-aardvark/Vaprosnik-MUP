2589ffcc8359c5eee1e54447dfdb3d86225479b6
arthas168 <pavlovskipetko@abv.bg>
2020-03-19 21:34:15 +0200

    Trying to add newline to question text.

--- src/components/GlobalState/actions.js
+++ src/components/GlobalState/actions.js
@@ -27,6 +27,10 @@ export const questionsUpdate = async () => {
       const { option1, option2, option3, option4, text } = element;
 
       if (text !== "kj") {
+        const textPatches = text.split("\\d\\.");
+
+        const newText = textPatches.join("\n");
+
         const index1 = option1.lastIndexOf(",");
         const index2 = option2.lastIndexOf(",");
         const index3 = option3.lastIndexOf(",");
@@ -51,7 +55,8 @@ export const questionsUpdate = async () => {
         const backPart4 = "@" + option4.substring(index4 + 1);
 
         element.option4 = frontPart4 + backPart4;
-        console.log(element);
+
+        element.text = newText;
         return element;
       }
 
