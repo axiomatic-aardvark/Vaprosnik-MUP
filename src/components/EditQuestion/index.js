a5b91137c3a45bb8fdc274dc86d16c5a87357549
arthas168 <pavlovskipetko@abv.bg>
2020-03-22 18:43:47 +0200

    Edit completely finalized.

--- src/components/EditQuestion/index.js
+++ src/components/EditQuestion/index.js
@@ -72,10 +72,18 @@ export default props => {
       .post(
         `https://server-vaprosnik.herokuapp.com/questions/update/${state.id}`,
         {
-          option1: option1.endsWith("t") ? option1 : option1 + ",t",
-          option2: option2.endsWith("f") ? option2 : option2 + ",f",
-          option3: option3.endsWith("f") ? option3 : option3 + ",f",
-          option4: option4.endsWith("f") ? option4 : option4 + ",f",
+          option1: option1.endsWith("t")
+            ? option1.substring(0, option1.length - 2) + ",t"
+            : option1 + ",t",
+          option2: option2.endsWith("f")
+            ? option2.substring(0, option2.length - 2) + ",f"
+            : option2 + ",f",
+          option3: option3.endsWith("f")
+            ? option3.substring(0, option3.length - 2) + ",f"
+            : option3 + ",f",
+          option4: option4.endsWith("f")
+            ? option4.substring(0, option4.length - 2) + ",f"
+            : option4 + ",f",
           text: text
         }
       )
